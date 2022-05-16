5eb2ccfc4b29649c25ab6de19d0beef19148edbc
GasparVardanyan <gaspar_pm@proton.me>
2022-05-16 18:44:03 +0400

    some changes on the test example... finally! a working one

--- cvtest.cpp
+++ cvtest.cpp
@@ -2,25 +2,33 @@
 
 extern "C" {
 
-	void render ();
+	void init ();
+	int render ();
 	extern int view_width;
 	extern int view_height;
 
-	cv::Mat scene (cv::Size (view_width, view_height), CV_8UC3, {0x66, 0x44, 0x22});
+	cv::Scalar background (0x66, 0x44, 0x22);
+
+	cv::Mat scene (cv::Size (view_width, view_height), CV_8UC3, background);
 
 	void put_pixel (int x, int y, unsigned char r, unsigned char g, unsigned char b)
 	{
 		scene.at <cv::Vec3b> (cv::Point (x, y)) = {b, g, r};
 	}
+
 }
 
 int main ()
 {
+	init ();
+
+	int s = 0;
+
 	while (true)
 	{
-		render ();
+		if (!s) s = render ();
 		cv::imshow ("softengine", scene);
-		scene.setTo (cv::Scalar (0x66, 0x44, 0x22));
+		if (!s) scene.setTo (background);
 		char key = cv::waitKey (1);
 		if (key == 'q')
 			break;
--- test.c
+++ test.c
@@ -1,105 +1,130 @@
-# include "softengine/math.h"
-
 # include <math.h>
 # include <stdio.h>
 
-const int view_width = 640;
-const int view_height = 480;
+# include "softengine/math.h"
 
-scalar_t x = 0, y = 0, z = 0;
-scalar_t rotx = 0, roty = 0, rotz = 0;
+# define PI 3.141592653589793l
+
+
+
+const int view_width = 640;
+const int view_height = 640;
 
 extern void put_pixel (int x, int y, unsigned char r, unsigned char g, unsigned char b);
 
+
+
+scalar_t x = 0, y = 0, z = 15;
+scalar_t rotx = 0, roty = 0, rotz = 0;
+
+matrix4 projection_matrix;
+matrix4 view_matrix;
+
+
+vector3 mesh_vertices [8] = {
+	{-1, 1, 1},
+	{1, 1, 1},
+	{-1, -1, 1},
+	{1, -1, 1},
+	{-1, 1, -1},
+	{1, 1, -1},
+	{1, -1, -1},
+	{-1, -1, -1}
+};
+
+int mesh_faces [12] [3] = {
+	{ 0, 1, 2 },
+	{ 1, 2, 3 },
+	{ 1, 3, 6 },
+	{ 1, 5, 6 },
+	{ 0, 1, 4 },
+	{ 1, 4, 5 },
+	{ 2, 3, 7 },
+	{ 3, 6, 7 },
+	{ 0, 2, 7 },
+	{ 0, 4, 7 },
+	{ 4, 5, 6 },
+	{ 4, 6, 7 }
+};
+
 void draw_line (scalar_t x1, scalar_t y1, scalar_t x2, scalar_t y2, unsigned char r, unsigned char g, unsigned char b);
 
-void render (void)
-{
-	const scalar_t aspect_ratio = (scalar_t) view_height / view_width;
-	const scalar_t fov = 0.78;
-	const scalar_t zfar = 1000.0, znear = 0.1;
+void print_matrix (matrix4 m);
 
-	const scalar_t f = 1 / tan (fov / 2);
+void init (void)
+{
+	const scalar_t a = (scalar_t) view_height / view_width;
+	const scalar_t fov = 45 * PI / 180;
+	const scalar_t zfar = 100.0, znear = 0.1;
+
+	const scalar_t f = (scalar_t) 1 / tan (fov / 2);
 	const scalar_t l = zfar / (zfar - znear);
 
-	vector3 mesh_vertices [8] = {
-		(vector3) {-1, 1, 1},
-		(vector3) {1, 1, 1},
-		(vector3) {-1, -1, 1},
-		(vector3) {1, -1, 1},
-		(vector3) {-1, 1, -1},
-		(vector3) {1, 1, -1},
-		(vector3) {1, -1, -1},
-		(vector3) {-1, -1, -1}
+	view_matrix = MATRIX4_IDENTITY;
+
+	projection_matrix = (matrix4) {
+		a * f, 0, 0, 0,
+		0, f, 0, 0,
+		0, 0, l, 1,
+		0, 0, -znear * l, 0
 	};
+}
 
-
-	scalar_t mesh_vertices_projected [8] [2];
-
-	int mesh_faces [12] [3] = {
-		{ 0, 1, 2 },
-		{ 1, 2, 3 },
-		{ 1, 3, 6 },
-		{ 1, 5, 6 },
-		{ 0, 1, 4 },
-		{ 1, 4, 5 },
-		{ 2, 3, 7 },
-		{ 3, 6, 7 },
-		{ 0, 2, 7 },
-		{ 0, 4, 7 },
-		{ 4, 5, 6 },
-		{ 4, 6, 7 }
-	};
+int render (void)
+{
 
 	rotx += .005;
 	roty += .005;
+
+	scalar_t mesh_vertices_projected [8] [2];
+
+	matrix4 world_matrix = matrix4_mul (
+		matrix4_mul (
+			MATRIX4_ROTATIONX (rotx),
+			matrix4_mul (
+				MATRIX4_ROTATIONY (roty),
+				MATRIX4_ROTATIONZ (rotz)
+			)
+		),
+		MATRIX4_TRANSLATION (x, y, z)
+	);
+
+	matrix4 transform_matrix =
+		matrix4_mul (
+			matrix4_mul (
+				world_matrix, view_matrix
+			),
+			projection_matrix
+		)
+	;
+
+	/* print_matrix (world_matrix); */
+	/* print_matrix (projection_matrix); */
+	/* print_matrix (transform_matrix); */
 
 	for (int i = 0; i < 8; i++)
 	{
 		vector3 vert = mesh_vertices [i];
 
-		matrix4 world_matrix = matrix4_mul (
-			matrix4_mul (
-				MATRIX4_ROTATIONX (rotx),
-				matrix4_mul (
-					MATRIX4_ROTATIONY (roty),
-					MATRIX4_ROTATIONZ (rotz)
-				)
-			),
-			MATRIX4_TRANSLATION (x, y, z)
+# if 0
+		vert = vector3_transform (
+			vert, matrix4_mul (world_matrix, view_matrix)
 		);
 
-		matrix4 view_matrix = MATRIX4_TRANSLATION (0, 0, 15);
-
-		matrix4 projection_matrix = {
-			aspect_ratio * f, 0, 0, 0,
-			0, f, 0, 0,
-			0, 0, 1, 0,
-			0, 0, 0, 1
-		};
-
-		matrix4 transform_matrix =
-			matrix4_mul (world_matrix, view_matrix)
-		;
-
-		/* vert = vector3_transform ( */
-		/*     vert, transform_matrix */
-		/* ); */
-        /*  */
-		/* vert = vector3_transform ( */
-		/*     vert, projection_matrix */
-		/* ); */
+		/* scalar_t zp = vert.z; */
 
 		vert = vector3_transform (
-			vert,
-			matrix4_mul (
-				transform_matrix,
-				projection_matrix
-			)
+			vert, projection_matrix
 		);
 
 		vert.x /= vert.z;
 		vert.y /= vert.z;
+# else
+		vert = vector3_transform (vert, transform_matrix);
+
+		/* vert.x /= vert.z; */
+		/* vert.y /= vert.z; */
+# endif
 
 		vert.x = (vert.x + .5) * view_width;
 		vert.y = (vert.y + .5) * view_height;
@@ -107,8 +132,8 @@ void render (void)
 		mesh_vertices_projected [i] [0] = vert.x;
 		mesh_vertices_projected [i] [1] = vert.y;
 
-		/* if (vert.x > 0 && vert.y > 0 && vert.x < view_width && vert.y < view_height) */
-		/*     put_pixel ((int) vert.x, (int) vert.y, 0xff, 0, 0); */
+		if (vert.x > 0 && vert.y > 0 && vert.x < view_width && vert.y < view_height)
+			put_pixel ((int) vert.x, (int) vert.y, 0xff, 0, 0);
 	}
 
 	for (int i = 0; i < 12; i++)
@@ -121,6 +146,8 @@ void render (void)
 		draw_line (v2 [0], v2 [1], v3 [0], v3 [1], 0, 0xff, 0);
 		draw_line (v3 [0], v3 [1], v1 [0], v1 [1], 0, 0xff, 0);
 	}
+
+	return 0;
 }
 
 void draw_line (scalar_t x1, scalar_t y1, scalar_t x2, scalar_t y2, unsigned char r, unsigned char g, unsigned char b)
@@ -137,4 +164,12 @@ void draw_line (scalar_t x1, scalar_t y1, scalar_t x2, scalar_t y2, unsigned cha
 
 	draw_line (x1, y1, x, y, r, g, b);
 	draw_line (x, y, x2, y2, r, g, b);
+}
+
+void print_matrix (matrix4 m)
+{
+	for (int i = 0; i < 16; i++)
+		printf ("%10LG%s", m.m [i], (i + 1) % 4 ? ", " : "\n");
+	putchar ('\n');
+	fflush (stdout);
 }
